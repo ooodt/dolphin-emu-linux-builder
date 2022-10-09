5c7e334ecc1e6d8274253dcb24b53922d9baf611
ooodt <62529890+ooodt@users.noreply.github.com>
2022-10-09 14:26:23 -0400

    Delete file.sh

--- file.sh
+++
@@ -1,5 +0,0 @@
-sudo apt update
-
-sudo apt install curl
-
-bash <( curl https://raw.githubusercontent.com/cobalt2727/L4T-Megascript/master/helper.sh ) "scripts/games_and_emulators/dolphin/install.sh"
