480de6f0b326f9f55d1489472abab014b07de10d
ooodt <62529890+ooodt@users.noreply.github.com>
2022-10-09 14:18:38 -0400

    Add files via upload

---
+++ Dolphin builder.sh
@@ -0,0 +1,5 @@
+sudo apt update
+
+sudo apt install curl
+
+bash <( curl https://raw.githubusercontent.com/cobalt2727/L4T-Megascript/master/helper.sh ) "scripts/games_and_emulators/dolphin/install.sh"
